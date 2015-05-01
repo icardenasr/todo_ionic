11bc882721926cd16a76673acf75a0b21d518cf4
Israel Cardenas <israel.cardenas@gmail.com>
2015-05-01 19:34:49 +0200

    Primera version de TodoApp en Ionic

---
+++ www/js/app.js
@@ -0,0 +1,203 @@
+// Ionic Todo Improved App
+
+var app = angular.module('starter', ['ionic']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+// Factoria para almacen de datos de Proyectos
+app.factory("Projects", function() {
+  return {
+    // Devuelve todos los proyectos
+    all: function() {
+      var projectString = window.localStorage['projects'];
+      if(projectString) {
+        return angular.fromJson(projectString);
+      }
+      return [];
+    },
+    // Guarda todos los proyectos con sus tareas
+    save: function(projects) {
+      window.localStorage['projects'] = angular.toJson(projects);
+    },
+    // Crea un nuevo objeto proyecto (solo crea el objeto, no lo guarda)
+    newProject: function(projectTitle) {
+      // Add a new project
+      return {
+        title: projectTitle,
+        tasks: []
+      };
+    },
+    // Devuelve el índice del ultimo proyecto activo
+    getLastActiveIndex: function() {
+      return parseInt(window.localStorage['lastActiveProject']) || 0;
+    },
+    // Guarda el indice del ultimo proyecto activo
+    setLastActiveIndex: function(index) {
+      window.localStorage['lastActiveProject'] = index;
+    }
+  };
+});
+
+// Controlador para la pagina principal
+app.controller('ContentController', ['$scope', '$ionicSideMenuDelegate', '$ionicModal', 'Projects', '$timeout',
+  function($scope, $ionicSideMenuDelegate, $ionicModal, Projects, $timeout) {
+
+    // Variable que controla el borrado de las tareas y proyectos
+    $scope.showDeleteTasks = false;
+    $scope.showDeleteProjects = false;
+
+    // Inicializamos los proyectos
+    $scope.projects = Projects.all();
+
+    // Inicializamos el proyecto activo como el ultimo que hubo, o el primero
+    $scope.activeProject = $scope.projects[Projects.getLastActiveIndex()];
+
+    // Inicializamos la ventana modal para la creación de una nueva tarea
+    $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+      $scope.taskModal = modal;
+    }, {
+      scope: $scope,
+      animation: 'slide-in-up'
+    });
+
+    // Funcion que permite mostrar la ventana modal para crear nueva tarea
+    $scope.newTask = function() {
+      $scope.taskModal.show();
+    };
+
+    // Funcion que permite cerrar la ventana modal para crear nueva tarea
+    $scope.closeNewTask = function() {
+      $scope.taskModal.hide();
+    };
+
+    // Funcion que permite crear una nueva tarea desde la ventana modal
+    $scope.createTask = function(task) {
+      // Si no hay proyecto activo o la tarea esta vacia no se hace nada
+      if(!$scope.activeProject || !task) {
+        return;
+      }
+      // Deshabilitamos el estado de borrar tareas
+      $scope.showDeleteTasks = false;
+      // Introducimos la nueva tarea al final de las tareas del proyecto
+      $scope.activeProject.tasks.push({
+        title: task.title
+      });
+      // Ocultamos la ventana modal
+      $scope.taskModal.hide();
+      // Guardamos todos los proyectos (ineficiente!)
+      Projects.save($scope.projects);
+      // Reseteamos al titulo de la tarea para que salga vacio para la proxima
+      task.title = "";
+    };
+
+    // Funcion que muestra el menu de la izquierda
+    $scope.toggleProjects = function() {
+      $ionicSideMenuDelegate.toggleLeft();
+    };
+
+    // Funcion que permite crear un nuevo proyecto mediante un aviso
+    $scope.newProject = function() {
+      // Solicitamos el nombre del nuevo proyecto
+      var projectTitle = prompt('Project name');
+      if(projectTitle) {
+        createProject(projectTitle);
+      }
+    };
+
+    // Funcion que crea y guarda un nuevo proyecto dado su titulo
+    var createProject = function(projectTitle) {
+      // Deshabilitamos el estado de borrar proyectos
+      $scope.showDeleteProjects = false;
+      // Se crea el objeto del proyecto haciendo uso de la factoria
+      var newProject = Projects.newProject(projectTitle);
+      // Introducimos el nuevo proyecto en la variable del scope que los guarda
+      $scope.projects.push(newProject);
+      // Almacenamos dicha variable en el almacenamiento local mediante la factoria
+      Projects.save($scope.projects);
+      // Seleccionamos el proyecto creado como activo
+      $scope.selectProject(newProject, $scope.projects.length-1);
+    }
+
+    // Funcion que selecciona un proyecto como activo y cierra el menu lateral
+    $scope.selectProject = function(project, index) {
+      $scope.activeProject = project;
+      Projects.setLastActiveIndex(index);
+      $ionicSideMenuDelegate.toggleLeft(false);
+    };
+
+    // Funcion que muestra u oculta el borrado de las tareas
+    $scope.toggleDeleteTasks = function() {
+      $scope.showDeleteTasks = !$scope.showDeleteTasks;
+    }
+
+    // Funcion que muestra u oculta el borrado de los proyectos
+    $scope.toggleDeleteProjects = function() {
+      $scope.showDeleteProjects = !$scope.showDeleteProjects;
+    }
+
+    // Funcion que permite el borrado de una tarea
+    $scope.deleteTask = function(index) {
+      $scope.activeProject.tasks.splice(index, 1);
+      Projects.save($scope.projects);
+    }
+
+    // Funcion que permite el borrado de un proyecto
+    $scope.deleteProject = function(index) {
+      // Borramos el proyecto elegido
+      $scope.projects.splice(index, 1);
+      Projects.save($scope.projects);
+      // Comprobamos si hay mas proyectos
+      if($scope.projects.length == 0) {
+        $scope.forceOneProject();
+      } else {
+        // Quedan proyectos - Comprobamos si el proyecto borrado es el activo
+        if (index == Projects.getLastActiveIndex()) {
+          // El proyecto eliminado es el que esta activo
+          if (index == 0) {
+            // Nos movemos al siguiente
+            $scope.activeProject = $scope.projects[index];
+            Projects.setLastActiveIndex(index);
+          } else {
+            // Nos movemos al anterior
+            $scope.activeProject = $scope.projects[index-1];
+            Projects.setLastActiveIndex(index-1);
+          }
+        }
+      }
+    }
+
+    // Funcion que permite cambiar el orden de una tarea
+    $scope.reorderTask = function(task, fromIndex, toIndex) {
+      $scope.activeProject.tasks.splice(fromIndex, 1);
+      $scope.activeProject.tasks.splice(toIndex, 0, task);
+      Projects.save($scope.projects);
+    }
+
+    // Comprobacion inicial de si existe algun proyecto, y si no, se pide crear
+    $timeout(function() {
+      if($scope.projects.length == 0) {
+        $scope.forceOneProject();
+      }
+    });
+
+    // Funcion que fuerza la creacion de un primer proyecto si no hay ninguno
+    $scope.forceOneProject = function() {
+      while(true) {
+        var projectTitle = prompt('Your first project title:');
+        if(projectTitle) {
+          createProject(projectTitle);
+          break;
+        }
+      }
+    }
+
+  }]);
